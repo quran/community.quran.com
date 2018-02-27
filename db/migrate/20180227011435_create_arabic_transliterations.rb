cab1d860dda616c1dc1254768a040418ef4af242
Naveed Ahmad <naveedahmada036@gmail.com>
2018-02-27 07:58:00 +0500

    lets use this oppournity and finish indo pak wbw

--- db/migrate/20180227011435_create_arabic_transliterations.rb
+++ db/migrate/20180227011435_create_arabic_transliterations.rb
@@ -3,6 +3,7 @@ class CreateArabicTransliterations < ActiveRecord::Migration[5.1]
     create_table :arabic_transliterations do |t|
       t.belongs_to :word, index: true
       t.string :text
+      t.string :indopak_text
 
       t.timestamps
     end
