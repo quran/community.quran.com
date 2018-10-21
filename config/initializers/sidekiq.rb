14cca927994494ceeb5fc8d6b40bd976ff1f7207
Naveed Ahmad <naveedahmada036@gmail.com>
2018-10-21 05:00:23 +0500

    sidekiq isn't picking redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,12 +2,12 @@
 Sidekiq::Extensions.enable_delay!
 Sidekiq.logger.level = Logger::WARN
 
-redis_url = ENV.fetch('REDIS_URL') {'localhost:6379'}
+# redis_url = ENV.fetch('REDIS_URL') {'localhost:6379'}
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
-end
+# Sidekiq.configure_server do |config|
+#  config.redis = { url: redis_url }
+#end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
-end
+#Sidekiq.configure_client do |config|
+#  config.redis = { url: redis_url }
+#end
