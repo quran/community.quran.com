73c06db1614d76e53e31ec95ff80fb44b7b10043
Naveed Ahmad <naveedahmada036@gmail.com>
2019-11-19 23:57:47 +0500

    added support for exporting translation with footnote

--- app/jobs/export_translation_job.rb
+++ app/jobs/export_translation_job.rb
@@ -1,10 +1,12 @@
 class ExportTranslationJob < ApplicationJob
   queue_as :default
   STORAGE_PATH = "public/assets/exported_databses"
+  SEE_MORE_REF_REGEXP = Regexp.new('(?<ref>\d+:\d+)')
+  TAG_SANITIZER = Rails::Html::WhiteListSanitizer.new
 
   def perform(resource_id, original_file_name)
     resource_content = ResourceContent.find(resource_id)
-    
+
     file_name = (original_file_name.presence || resource_content).chomp('.db')
 
     file_path = "#{STORAGE_PATH}/#{Time.now.to_i}"
@@ -33,23 +35,42 @@ class ExportTranslationJob < ApplicationJob
   def prepare_import_sql(resource)
     if resource.tafisr?
       Tafsir.where(resource_content: resource).map do |tafsir|
-        verse_key =  tafsir.verse_key.split(':')
+        verse_key = tafsir.verse_key.split(':')
         text = ExportRecord.connection.quote(tafsir.text)
         "(#{verse_key[0]}, #{verse_key[1]}, #{text})"
       end.join(',')
     else
       Verse.eager_load(:translations).
-        where('translations.resource_content_id': resource.id)
-        .map do |v|
-          translation = ExportRecord.connection.quote(v.translations.first.text)
-          "(#{v.chapter_id}, #{v.verse_number}, #{translation})"
-        end.join(',')
+          where('translations.resource_content_id': resource.id)
+          .map do |v|
+        translation = format_translation_text(v.translations.first)
+        "(#{v.chapter_id}, #{v.verse_number}, #{translation})"
+      end.join(',')
     end
   end
 
+  def format_translation_text(translation)
+    text = translation.text
+
+    translation.foot_notes.each do |f|
+      reg = /<sup foot_note=#{f.id}>\d+<\/sup>/
+
+      text = text.gsub(reg) do
+        "[[#{f.text}]]"
+      end
+    end
+
+    sanitized = TAG_SANITIZER.sanitize(text.to_s.strip, tags: %w(), attributes: []).gsub(/[\r\n]+/, "<br/>")
+    sanitized = sanitized.gsub(SEE_MORE_REF_REGEXP) do
+      "{#{Regexp.last_match(1)}}"
+    end
+
+    ExportRecord.connection.quote(sanitized)
+  end
+
   def connection_config(file_name)
-    { adapter: 'sqlite3',
-      database: file_name
+    {adapter: 'sqlite3',
+     database: file_name
     }
   end
 
