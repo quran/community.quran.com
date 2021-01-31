9f759e62baedaafafc9565b5177488c9cc35d2ae
Naveed Ahmad <naveedahmada036@gmail.com>
2021-01-31 14:16:00 +0500

    validates

--- app/models/important_note.rb
+++ app/models/important_note.rb
@@ -1,5 +1,5 @@
 class ImportantNote < ApplicationRecord
-  validate :title, :text, presence: true
+  validates :title, :text, presence: true
 
   belongs_to :verse, optional: true
   belongs_to :word, optional: true
