972e651fa23c680e7217c857349236b279d77b0f
Naveed Ahmad <naveedahmada036@gmail.com>
2021-02-24 11:49:01 +0500

    added index

--- app/admin/muhsaf_page.rb
+++ app/admin/muhsaf_page.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register MuhsafPage do
   menu parent: "Quran", priority: 6
   actions :all, except: [:destroy, :new]
+
+  index do
+    id_column
+
+    column :first_verse_id
+    column :last_verse_id
+    column :verses_count
+
+    actions
+  end
 end
